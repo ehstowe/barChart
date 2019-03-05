502abc752c932596af41ec0258e4e603912568a8
Emma H. Stowe <emma.stowe@centre.edu>
2019-03-05 15:39:50 -0500

    3

--- colors.js
+++ colors.js
@@ -1,6 +1,60 @@
 var dataP = d3.json("colors.json");
+
+var drawChart=function(colors)
+{
+var width=400;
+var height=200;
+var barWidth=width/colors.length;
+
+var svg=
+d3.select("svg")
+.attr("width", width)
+.attr("height", height)
+
+svg.selectAll("rect")
+.data(colors)
+.enter()
+.append("rect")
+.attr("x", function(d,i)
+  {return i*barWidth;})
+.attr("y", function(d)
+  {return height-d.num*10})
+.attr("width", barWidth)
+.attr("height", function(d)
+  {return d.num*10})
+.attr("fill", function(d)
+  {return d.color;})}
+
+
+var drawLabels=function(colors)
+{
+  var width=400;
+  var height=200;
+  var barWidth=width/colors.length;
+
+  var svg=
+  d3.select("svg")
+  .attr("width", width)
+  .attr("height", height)
+svg.selectAll("text")
+.data(colors)
+.enter()
+.append("text")
+.text(function(d){
+  return d.num;})
+.attr("x", function(d,i){
+  return i * (width / colors.length) +25;
+})
+.attr("y", function(d){
+  return height-(d.num*10)+20;
+})
+.attr("fill", "white");
+}
+
 dataP.then(function(data)
 {console.log("data", data);
+drawChart(data);
+drawLabels(data);
 },
 function(err){console.log(err);}
 )
