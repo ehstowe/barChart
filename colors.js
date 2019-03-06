1419dd7b1b752a8ea51744d6b73c31392705c183
Emma H. Stowe <emma.stowe@centre.edu>
2019-03-05 21:36:15 -0500

    936

--- colors.js
+++ colors.js
@@ -18,10 +18,10 @@ svg.selectAll("rect")
 .attr("x", function(d,i)
   {return i*barWidth;})
 .attr("y", function(d)
-  {return height-d.num*10})
+  {return height-d.num*20})
 .attr("width", barWidth)
 .attr("height", function(d)
-  {return d.num*10})
+  {return d.num*20})
 .attr("fill", function(d)
   {return d.color;})}
 
@@ -46,15 +46,27 @@ svg.selectAll("text")
   return i * (width / colors.length) +25;
 })
 .attr("y", function(d){
-  return height-(d.num*10)+20;
+  return height-(d.num*10)+7;
 })
 .attr("fill", "white");
 }
 
 dataP.then(function(data)
-{console.log("data", data);
+{
 drawChart(data);
 drawLabels(data);
+createLegend(data);
 },
 function(err){console.log(err);}
 )
+
+var createLegend=function(data){
+var table=d3.select("h1").append("table");
+var cols=table.selectAll("tr")
+.data(dataP)
+.enter()
+.append("tr");
+cols.append("td").text(function(d){return d.color})
+.style("background", function(d){
+  if (d.color=="blue"){return "blue"}
+})}
